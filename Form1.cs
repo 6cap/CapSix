21c404bd4e74aee9385720dc73b37f44192597e3
Ryogisan <78507813+Ryogisan@users.noreply.github.com>
2021-11-24 00:38:38 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Threading;
+using System.IO;
+using DlibDotNet;
+using DlibDotNet.Extensions;
+using Dlib = DlibDotNet.Dlib;
+
+namespace CapSix
+{
+    public partial class Form1 : Form
+    {
+        private readonly VideoCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var bgWorker = (BackgroundWorker)sender;
+            while (!bgWorker.CancellationPending)
+            {
+                using (var frameMat = capture.RetrieveMat())
+                {
+                    var frameBitmap = BitmapConverter.ToBitmap(frameMat);
+                    bgWorker.ReportProgress(0, frameBitmap);
+                }
+                Thread.Sleep(100);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            capture.Open(0, VideoCaptureAPIs.ANY);
+            if (!capture.IsOpened())
+            {
+                Close();
+                return;
+            }
+            ClientSize = new System.Drawing.Size(capture.FrameWidth, capture.FrameHeight);
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            capture.Dispose();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var frameBitmap = (Bitmap)e.UserState;
+            pictureBox1.Image?.Dispose();
+            pictureBox1.Image = frameBitmap;
+        }
+    }
+}
